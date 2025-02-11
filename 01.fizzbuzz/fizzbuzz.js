cf47419527a38b917f954a477f7dc501eb714144
takao0206 <takao.haruna.0206@gmail.com>
2025-02-11 14:56:24 +0900

    FizzBuzzプログラムをJavaScriptで作成

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+let output = "";
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 15 === 0) {
+    output = "FizzBuzz";
+  } else if (i % 3 === 0) {
+    output = "Fizz";
+  } else if (i % 5 === 0) {
+    output = "Buzz";
+  } else {
+    output = i;
+  }
+  console.log(String(output));
+}
