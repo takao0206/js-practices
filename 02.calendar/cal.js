b5c5cf338531ef19b0b3f2a995e0180710a3fbde
takao0206 <takao.haruna.0206@gmail.com>
2025-05-03 15:32:20 +0900

    date-fnsからインポートされたことを明確するために、名前空間を使ったインポートに変更。

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { startOfMonth, endOfMonth, format, addDays } from "date-fns";
+import * as dateFns from "date-fns";
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
@@ -8,21 +8,21 @@ const now = new Date();
 const year = args.y || now.getFullYear();
 const month = args.m || now.getMonth() + 1;
 
-const header = format(new Date(year, month - 1), "MMMM yyyy");
+const header = dateFns.format(new Date(year, month - 1), "MMMM yyyy");
 const weekDays = "Su Mo Tu We Th Fr Sa";
 const padding = " ".repeat(Math.ceil((weekDays.length - header.length) / 2));
 
 console.log(padding + header);
 console.log(weekDays);
 
-const firstDate = startOfMonth(new Date(year, month - 1));
-const lastDate = endOfMonth(new Date(year, month - 1));
+const firstDate = dateFns.startOfMonth(new Date(year, month - 1));
+const lastDate = dateFns.endOfMonth(new Date(year, month - 1));
 const spaceWidth = 3;
 const initialSpaceCounts = firstDate.getDay() * spaceWidth;
 
 process.stdout.write(" ".repeat(initialSpaceCounts));
 
-for (let date = firstDate; date <= lastDate; date = addDays(date, 1)) {
+for (let date = firstDate; date <= lastDate; date = dateFns.addDays(date, 1)) {
   process.stdout.write(String(date.getDate()).padStart(2, " "));
   if (date.getDay() === 6 || date.getDate() === lastDate.getDate()) {
     console.log();
