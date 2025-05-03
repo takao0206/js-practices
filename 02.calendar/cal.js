be556c80e7b88763add9adc77d02bed44589a2bd
takao0206 <takao.haruna.0206@gmail.com>
2025-05-03 16:28:09 +0900

    コードの重複があったので、解消した。

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -7,16 +7,17 @@ const arg = minimist(process.argv.slice(2));
 const now = new Date();
 const year = arg.y ?? now.getFullYear();
 const month = arg.m ?? now.getMonth() + 1;
+const targetMonthFirstDay = new Date(year, month - 1, 1);
 
-const header = dateFns.format(new Date(year, month - 1), "MMMM yyyy");
+const header = dateFns.format(targetMonthFirstDay, "MMMM yyyy");
 const weekDays = "Su Mo Tu We Th Fr Sa";
 const padding = " ".repeat(Math.ceil((weekDays.length - header.length) / 2));
 
 console.log(padding + header);
 console.log(weekDays);
 
-const firstDate = dateFns.startOfMonth(new Date(year, month - 1));
-const lastDate = dateFns.endOfMonth(new Date(year, month - 1));
+const firstDate = dateFns.startOfMonth(targetMonthFirstDay);
+const lastDate = dateFns.endOfMonth(targetMonthFirstDay);
 const spaceWidth = 3;
 const initialSpaceCounts = firstDate.getDay() * spaceWidth;
 
